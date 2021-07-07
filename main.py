6bb16d0abfb32174d74247b5f7cbbeaabbe98ae0
Mark <markh5@uci.edu>
2021-07-06 17:57:23 -0700

    First commit of TileAdventure game

---
+++ main.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+'''
+I am creating a tile game, which has a 3 by 3 grid
+There are 4 different tiles
+    player
+    enemy
+    gold
+    equipment
+The player can move, and come in contact with enemies, gain exp. At level 5, a boss will spawn. Beat the boss to win
+'''
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
